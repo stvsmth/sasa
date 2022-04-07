288d4402f7a908c5dc281e2fb70888ce92b66388
stvsmth <stvsmth@gmail.com>
2022-04-07 15:48:02 -0600

    Put slides into loop.

--- src/main.rs
+++ src/main.rs
@@ -6,54 +6,44 @@ use crossterm::{
 };
 use std::io::{stdout, Write};
 
-const BOTTOM_OFFSET: u16 = 8;
+const BOTTOM_OFFSET: u16 = 4;
 
 fn main() -> Result<()> {
-    let mut stdout = stdout();
-
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
-
     let (x_max, y_max) = terminal::size()?;
-    
-    // TODO: Exit early if we are below some useful dimensions.
-    let y_max = y_max - BOTTOM_OFFSET; // Give some space for command prompt to reappear
-    for y in 0..y_max {
-        for x in 0..x_max {
-            if (y == 0 || y == y_max - 1) || (x == 0 || x == x_max - 1) {
-                stdout
-                    .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("█".magenta()))?;
-            } else if y == y_max / 2 {
-                stdout
-                    .queue(cursor::MoveTo(5, y))?
-                    .queue(style::Print("Hello ..."))?;
-            }
-        }
-    }
-    stdout.flush()?;
+    let y_max = y_max - BOTTOM_OFFSET;
 
-    match read()? {
-        Event::Key(_event) => (),
-        Event::Mouse(_event) => (),
-        Event::Resize(_width, _height) => (),
-    }
+    let input = vec!["Hello ...", "... World", "from", "Sasa"];
+    let mut lines = input.iter();
 
+    let mut stdout = stdout();
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
 
-    for y in 0..y_max {
-        for x in 0..x_max {
-            if (y == 0 || y == y_max - 1) || (x == 0 || x == x_max - 1) {
-                stdout
-                    .queue(cursor::MoveTo(x, y))?
-                    .queue(style::PrintStyledContent("█".white()))?;
-            } else if y == y_max / 2 {
-                stdout
-                    .queue(cursor::MoveTo(5, y))?
-                    .queue(style::Print("... World"))?;
+    loop {
+        match lines.next() {
+            None => break,
+            Some(line) => {
+                for y in 0..y_max {
+                    for x in 0..x_max {
+                        if (y == 0 || y == y_max - 1) || (x == 0 || x == x_max - 1) {
+                            stdout
+                                .queue(cursor::MoveTo(x, y))?
+                                .queue(style::PrintStyledContent("█".white()))?;
+                        } else if y == y_max / 2 {
+                            stdout
+                                .queue(cursor::MoveTo(5, y))?
+                                .queue(style::Print(line))?;
+                        }
+                    }
+                }
+                stdout.flush()?;
+                match read()? {
+                    Event::Key(_event) => (),
+                    Event::Mouse(_event) => (),
+                    Event::Resize(_width, _height) => (),
+                }
+                stdout.execute(terminal::Clear(terminal::ClearType::All))?;
             }
         }
     }
-    stdout.flush()?;
-
     Ok(())
 }
