10ceccd7bd53847ca44afc11cd1229d0ae29d456
stvsmth <stvsmth@gmail.com>
2022-04-07 11:41:05 -0600

    Add a second slide, sleep then draw.
    NOT a useful mechanic, but just moving the ball forward.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use crossterm::{
     terminal, ExecutableCommand, QueueableCommand, Result,
 };
 use std::io::{stdout, Write};
+use std::{thread, time};
 
 fn main() -> Result<()> {
     let mut stdout = stdout();
@@ -15,17 +16,36 @@ fn main() -> Result<()> {
     for y in 0..y_max {
         for x in 0..x_max {
             if (y == 0 || y == y_max - 1) || (x == 0 || x == x_max - 1) {
-                // in this loop we are more efficient by not flushing the buffer.
                 stdout
                     .queue(cursor::MoveTo(x, y))?
                     .queue(style::PrintStyledContent("█".magenta()))?;
             } else if y == y_max / 2 {
                 stdout
                     .queue(cursor::MoveTo(5, y))?
-                    .queue(style::Print("Hello World"))?;
+                    .queue(style::Print("Hello ..."))?;
             }
         }
     }
     stdout.flush()?;
+
+    let three_secs = time::Duration::from_millis(3000);
+
+    thread::sleep(three_secs);
+
+    for y in 0..y_max {
+        for x in 0..x_max {
+            if (y == 0 || y == y_max - 1) || (x == 0 || x == x_max - 1) {
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::PrintStyledContent("█".yellow()))?;
+            } else if y == y_max / 2 {
+                stdout
+                    .queue(cursor::MoveTo(5, y))?
+                    .queue(style::Print("... World"))?;
+            }
+        }
+    }
+    stdout.flush()?;
+
     Ok(())
 }
